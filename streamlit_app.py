bea80f8363a7c0b194b1b4efa9cf90b8c95f33d1
KillerWhale <baocnguyen2000@gmail.com>
2026-09-17 20:40:09 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -512,7 +512,7 @@ if legend_mode == "KillerWhale Way":
 
     st.markdown(
         """
-**Note: Auto Snip Order**  
+**Note: Auto Snipping Order**  
 **RE:** 30M-1G → 1G-6G → 1G-18G  
 **CE:** AC Line 1-20 → AC Line → IPMI0-20 → IPMI → ETH0-20 → Tel Line
         """
