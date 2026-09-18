b0c0e1db681aa70e20a8a8b33a29415da99b0da3
KillerWhale <baocnguyen2000@gmail.com>
2026-09-17 21:11:52 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ st.markdown(
     top: 50%;
     transform: translateY(-50%);
     width: 400px;
-    height: 100vh;
+    height: 80vh;
     background-image: url("./app/static/spiderman_side.gif");
     background-position: center;
     background-repeat: no-repeat;
