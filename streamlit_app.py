d26f7d9db3ae591bc6a82bdb1451eabcbfcf5ad4
KillerWhale <baocnguyen2000@gmail.com>
2026-09-17 17:56:20 -0700

    Fix radio button options for masking method

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ import killerwhale_backend as kw
 st.set_page_config(page_title="KillerWhale Extractor 3ThouWow", page_icon="🐋", layout="centered")
 st.markdown("""<h1 style="font-size:2.3rem;white-space:nowrap;">🐋 KillerWhale Extractor 3ThouWow 🐋</h1>""", unsafe_allow_html=True)
 
-legend_mode = st.radio("Masking method", ["#################### KillerWhale Way", "Manual Way"],
+legend_mode = st.radio("Masking method", "KillerWhale Way", "Manual Way"],
                        horizontal=True, label_visibility="collapsed")
 st.subheader("RE")
 re_files = st.file_uploader("RE PDF(s)", type=["pdf"], accept_multiple_files=True, key="re_files")
